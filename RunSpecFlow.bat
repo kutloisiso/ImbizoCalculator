f6a19efec2bd36460c164b456a9a00165b2426a2
kutloisiso <kutloisiso@gmail.com>
2012-12-10 22:06:10 +0200

    added runspecflow

--- RunSpecFlow.bat
+++ RunSpecFlow.bat
@@ -1,6 +1,6 @@
-"C:\Program Files (x86)\NUnit 2.6.2\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 Bowling.Specflow\bin\Debug\Bowling.Specflow.dll
+"C:\Program Files (x86)\NUnit 2.6.2\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 ImbizoCalculator.Spec\bin\Debug\ImbizoCalculator.Specs.dll
  
-"packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport Bowling.Specflow\Bowling.SpecFlow.csproj
+"packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport Bowling.Specflow\ImbizoCalculator.Specs.csproj
  
 IF NOT EXIST TestResult.xml GOTO FAIL
 IF NOT EXIST TestResult.html GOTO FAIL
